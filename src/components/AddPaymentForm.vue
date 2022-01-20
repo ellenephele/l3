9a654e120cb099a767e5afe0c819dd5c0d84463c
AnastasiiaRI <elle.nephele@gmail.com>
2022-01-20 17:07:02 +0300

    дз 3 урока без пагинации

---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <label for="value">
+      <input id="value" type="text" placeholder="Amount" v-model="value">
+    </label>
+    <label for="category">
+      <input id="category" type="text" placeholder="Type" v-model="category">
+    </label>
+    <label for="date">
+      <input id="date" type="text" placeholder="Date" v-model="date">
+    </label>
+    <button @click="addPayment">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddPaymentForm',
+  data: () => ({
+    value: '',
+    category: '',
+    date: '',
+  }),
+  methods: {
+    addPayment() {
+      // const data = {
+      //   value: this.value,
+      //   category: this.category,
+      //   date: this.date || this.paymentDay,
+      // };
+      const {
+        value, category, date, paymentDay,
+      } = this;
+      const data = {
+        value,
+        category,
+        date: date || paymentDay,
+      };
+
+      // console.log(data);
+      this.$emit('add-payment', data);
+    },
+  },
+  computed: {
+    paymentDay() {
+      const currentDate = new Date();
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+
+      return `${day}.${month}.${year}`;
+    },
+  },
+};
+</script>
+
+<style module>
+
+</style>
---
+++ src/components/PaymentDisplay.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<!--  <div v-if="show">Prop is true</div>-->
+<!--  {{ items }} {{ $attrs }}-->
+  <div v-for="(item, index) in items" :key="index">{{ index + 1 }} - {{ item }}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentDisplay',
+  props: {
+    items: {
+      type: Array,
+      default: () => ([]),
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
